c6b7aa55807a6b5a5cd34471018ab7cc4397b1e8
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2024-04-14 18:46:04 +0900

    Add files via upload

---
+++ 단계별로_풀어보기/1_입출력과_사칙연산/Main.java
@@ -0,0 +1,14 @@
+package Beakjoon;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	
+	}
+}
+	
